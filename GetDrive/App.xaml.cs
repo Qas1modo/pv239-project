ae08538a50b4d1d5c7071b71d029c4d761f43f96
radomir.mann <radomir.mann@bluesoft.cz>
2024-03-19 14:52:47 +0100

    Project init

---
+++ GetDrive/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GetDrive
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GetDrive/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GetDrive
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
