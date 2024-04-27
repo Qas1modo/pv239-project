2c1cf4a11b350a0410a507250d90750e0c6e25e1
Markéta Schlemmerová <x450666@fi.muni.cz>
2024-04-27 18:14:59 +0200

    feat: All rides page - basic structure

--- GetDrive/Views/AllRides.xaml.cs
+++ GetDrive/Views/AllRides.xaml.cs
@@ -2,22 +2,9 @@ namespace GetDrive.Views;
 
 public partial class AllRides : ContentPage
 {
-    int count = 0;
 
     public AllRides()
     {
         InitializeComponent();
-    }
-
-    private void OnCounterClicked(object sender, EventArgs e)
-    {
-        count++;
-
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
     }
 }
