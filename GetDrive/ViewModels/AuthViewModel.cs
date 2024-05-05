9d00685161e1a7ebef57041f53d0a27fa52b09c5
SamValastin <samuel.valastin@datasentics.com>
2024-05-05 18:51:01 +0200

    Add: refactored Auth page & add support for password change (s)

--- GetDrive/ViewModels/AuthViewModel.cs
+++ GetDrive/ViewModels/AuthViewModel.cs
@@ -1,114 +1,97 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using GetDrive.Clients;
+using GetDrive.Models;
+using AutoMapper;
 using GetDrive.Api;
+using GetDrive.Views;
 
 namespace GetDrive.ViewModels;
 
 public partial class AuthViewModel : ViewModelBase
 {
     private readonly IAuthClient _authClient;
+    private readonly IMapper _mapper;
 
     [ObservableProperty]
-    private RegistrationDTO registrationData = new();
+    private AuthModel auth = new();
 
-    [ObservableProperty]
-    private LoginDto loginData = new();
-
-    [ObservableProperty]
-    private bool isRegistering = false;
-
-    [ObservableProperty]
-    private bool isLoggedIn;
-
-    [ObservableProperty]
-    private string statusMessage;
-
-    [ObservableProperty]
-    private string toggleButtonText = "Switch to register form";
-
-    public bool IsNotLoggedIn => !isLoggedIn;
-
-    public bool IsNotRegistering => !isRegistering;
-
-
-    public AuthViewModel(IAuthClient authClient)
+    public AuthViewModel(IAuthClient authClient, IMapper mapper)
     {
         _authClient = authClient;
-        UpdateStatusMessage();
+        _mapper = mapper;
+        UpdateToggleButtonText();
     }
 
     public override async Task OnAppearingAsync()
     {
         await base.OnAppearingAsync();
-        CheckLoginStatus();
+        await CheckLoginStatus();
     }
 
-    private async void CheckLoginStatus()
+    private async Task CheckLoginStatus()
     {
-        var token = await SecureStorage.GetAsync("Token");
-        IsLoggedIn = !string.IsNullOrEmpty(token);
-        OnPropertyChanged(nameof(IsNotLoggedIn));
+        var token = await SecureStorage.Default.GetAsync("Token");
+        Auth.IsLoggedIn = !string.IsNullOrWhiteSpace(token);
+        UpdateToggleButtonText();
     }
 
+    [RelayCommand]
+    private void ToggleMode()
+    {
+        Auth.IsRegistering = !Auth.IsRegistering;
+        UpdateToggleButtonText();
+    }
+
+    private void UpdateToggleButtonText()
+    {
+        Auth.ToggleButtonText = Auth.IsRegistering ? "Switch to Login form" : "Switch to Register form";
+        Auth.StatusMessage = Auth.IsRegistering ? "Please fill in registration entries." : "Please fill in login entries.";
+    }
 
     [RelayCommand]
     private async Task RegisterAsync()
     {
-        if (!IsRegisterFormValid())
+        if (!Auth.Registration.IsFormValid())
         {
-            UpdateStatusMessage("Please check your registration inputs and try again.");
+            Auth.StatusMessage = "Please check your registration inputs and try again.";
             return;
         }
-        try
+        var registrationDTO = _mapper.Map<RegistrationDTO>(Auth.Registration);
+        var (response, errorMessage) = await _authClient.Register(registrationDTO);
+
+        if (!string.IsNullOrEmpty(errorMessage))
         {
-            var response = await _authClient.Register(RegistrationData);
-            if (response != null && response.Token != null)
-            {
-                UpdateStatusMessage("Registration has been successfully done... ");
-                await Task.Delay(1500);
-                await Shell.Current.GoToAsync("//ridelistview");
-            }
-            else
-            {
-                UpdateStatusMessage("Registration was unsuccessful. Please check your fields.");
-            }
+            Auth.StatusMessage = errorMessage;
         }
-        catch (Exception ex)
+        else if (response != null && response.Token != null)
         {
-            StatusMessage = $"Registration failed: {ex.Message}";
+            Auth.StatusMessage = "Registration successful.";
+            await Task.Delay(1500); 
+            await Shell.Current.GoToAsync("//ridelistview");
+        }
+        else
+        {
+            Auth.StatusMessage = "Unexpected issue occured during registration.";
         }
     }
 
     [RelayCommand]
-    private async Task LoginAsync()
+    public async Task LoginAsync()
     {
-        if (!IsLoginFormValid())
+        var loginDto = _mapper.Map<LoginDto>(Auth.Login);
+        var (authResponse, errorMessage) = await _authClient.Login(loginDto);
+
+        if (!string.IsNullOrEmpty(errorMessage))
         {
-            UpdateStatusMessage("Please check your login inputs and try again.");
-            return;
-        }
-        try
+            Auth.StatusMessage = errorMessage;
+        } 
+        else if (authResponse != null && authResponse.Token != null)
         {
-            var response = await _authClient.Login(LoginData);
-            if (response != null && response.Token != null && response.UserId > 0)
-            {
-                await SecureStorage.SetAsync("Token", response.Token);
-                await SecureStorage.SetAsync("UserId", response.UserId.ToString());
-                IsLoggedIn = true;
-                OnPropertyChanged(nameof(IsNotLoggedIn));
-                UpdateStatusMessage("Login successful.");
-                await Task.Delay(1500);
-                await Shell.Current.GoToAsync("//ridelistview");
-            }
-            else
-            {
-                UpdateStatusMessage("Login failed. Please check your email and password.");
-            }
-        }
-        catch (Exception ex)
-        {
-            StatusMessage = $"Login failed: {ex.Message}";
+            Auth.IsLoggedIn = true;
+            Auth.StatusMessage = "Login successful.";
+            await Task.Delay(1500);
+            await Shell.Current.GoToAsync("//ridelistview");
         }
     }
 
@@ -116,42 +99,33 @@ public partial class AuthViewModel : ViewModelBase
     private async Task LogoutAsync()
     {
         await _authClient.Logout();
-        IsLoggedIn = false;
-        OnPropertyChanged(nameof(IsNotLoggedIn));
-        UpdateStatusMessage("Successful logout.");
-        await Task.Delay(1500);
+        Auth.IsLoggedIn = false;
         await Shell.Current.GoToAsync("//ridelistview");
     }
 
+
     [RelayCommand]
-    private void ToggleMode()
+    public async Task ShowChangePasswordPopup()
     {
-        IsRegistering = !IsRegistering;
-        OnPropertyChanged(nameof(IsNotRegistering));
-        UpdateStatusMessage();
-        ToggleButtonText = IsNotRegistering ? "Switch to register form" : "Switch to login form";
+        var changePasswordModalPage = new ChangePassword(this);
+        await Application.Current.MainPage.Navigation.PushModalAsync(changePasswordModalPage);
     }
 
-    private void UpdateStatusMessage(string? message = null)
+    [RelayCommand]
+    public async Task ChangePasswordAsync()
     {
-        StatusMessage = message ?? (IsRegistering
-                                    ? "Please fill in registration entries (all entries are required)."
-                                    : "Please fill in login entries.");
+        if (!Auth.ChangePassword.IsValid())
+        {
+            Auth.ChangePasswordStatusMessage = "Check your password inputs and try again.";
+            return;
+        }
+
+        var changePasswordDTO = _mapper.Map<ChangePasswordDTO>(Auth.ChangePassword);
+        var result = await _authClient.ChangePassword(changePasswordDTO);
+        Auth.ChangePasswordStatusMessage = result;
+        await Task.Delay(1000);
     }
 
-    private bool IsRegisterFormValid()
-    {
-        return !string.IsNullOrWhiteSpace(RegistrationData.Name) &&
-               !string.IsNullOrWhiteSpace(RegistrationData.Email) &&
-               !string.IsNullOrWhiteSpace(RegistrationData.Phone) &&
-               !string.IsNullOrWhiteSpace(RegistrationData.Password) &&
-               RegistrationData.Password == RegistrationData.RepeatPassword &&
-               RegistrationData.Password.Length >= 8;
-    }
-
-    private bool IsLoginFormValid()
-    {
-        return !string.IsNullOrWhiteSpace(LoginData.Email) &&
-               !string.IsNullOrWhiteSpace(LoginData.Password);
-    }
+    [RelayCommand]
+    public async Task HideChangePasswordModal() => await Application.Current.MainPage.Navigation.PopModalAsync();
 }
